140b8b38173f6a6e537bdf5d4ccf0cc0ec195737
SPashaS <SPavel.LD@gmail.com>
2022-02-28 13:05:49 +0300

    advantages + header

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -24,5 +24,22 @@ window.onload = function () {
             document.querySelector('.search-form').classList.remove('_active');
         }
     }
+
+    //header
+    const headerElement = document.querySelector('.header');
+    const callback = function (entries, observer) {
+        if(entries[0].isIntersecting) {
+            headerElement.classList.remove('_scroll')
+        } else {
+            headerElement.classList.add('_scroll')
+        }
+    };
+
+    const headerObserver = new IntersectionObserver(callback);
+    headerObserver.observe(headerElement);
+
 }
+
+
+
 
