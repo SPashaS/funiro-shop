b616a2d5a5c035cbd9737fafeda0705e9d62e788
SPashaS <SPavel.LD@gmail.com>
2022-03-09 23:33:50 +0300

    slider fix

--- src/js/files/gallery.js
+++ src/js/files/gallery.js
@@ -36,24 +36,39 @@ import '@scss/libs/gallery/lightgallery.scss';
 // import '@scss/libs/gallery/lightgallery-bundle.scss';
 
 // Запуск
-const galleries = document.querySelectorAll('[data-gallery]');
+const galleries = document.querySelectorAll('.furniture__row');
+
 if (galleries.length) {
-	let galleyItems = [];
+	let galleryItems = [];
 	galleries.forEach(gallery => {
-		galleyItems.push({
-			gallery,
-			galleryClass: lightGallery(gallery, {
-				//plugins: [lgZoom, lgThumbnail],
-				licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
-				speed: 500,
+			galleryItems.push({
+				gallery,
+				galleryClass: lightGallery(gallery, {
+					// plugins: [lgZoom, lgThumbnail],
+					licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
+					speed: 500,
+					// thumbnail: true,
+				})
 			})
-		})
-	});
+		});
 	// Добавляем в объект модулей
-	flsModules.gallery = galleyItems;
+	flsModules.gallery = galleryItems;
 }
 
 
-
-
-
+//Gallery
+// let gallery = document.querySelectorAll('._gallery');
+// if (gallery) {
+// 	gallery_init();
+// }
+// function gallery_init() {
+// 	for (let index = 0; index < gallery.length; index++) {
+// 		const el = gallery[index];
+// 		lightGallery(el, {
+// 			licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
+// 			counter: false,
+// 			selector: 'a',
+// 			download: false
+// 		});
+// 	}
+// }
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -278,6 +278,56 @@ window.onload = function () {
     }
 
 
+	// Furniture Gallery
+	const furniture = document.querySelector('.furniture__body');
+	if (furniture && !isMobile.any()) {
+		const furnitureItems = document.querySelector('.furniture__items');
+		const furnitureColumn = document.querySelectorAll('.furniture__column');
+
+		// Скорость анимации
+		const speed = furniture.dataset.speed;
+
+		// Объявление переменных
+		let positionX = 0;
+		let coordXprocent = 0;
+
+		function setMouseGalleryStyle() {
+			let furnitureItemsWidth = 0;
+			furnitureColumn.forEach(element => {
+				furnitureItemsWidth += element.offsetWidth;
+			});
+
+			const furnitureDifferent = furnitureItemsWidth - furniture.offsetWidth;
+			const distX = Math.floor(coordXprocent - positionX);
+
+			positionX = positionX + (distX * speed);
+			let position = furnitureDifferent / 200 * positionX;
+
+			furnitureItems.style.cssText = `transform: translate3d(${-position}px,0,0);`;
+
+			if (Math.abs(distX) > 0) {
+				requestAnimationFrame(setMouseGalleryStyle);
+			} else {
+				furniture.classList.remove('_init');
+			}
+		}
+		furniture.addEventListener("mousemove", function (e) {
+			// Получение ширины
+			const furnitureWidth = furniture.offsetWidth;
+
+			// Ноль по середине
+			const coordX = e.pageX - furnitureWidth / 2;
+
+			// Получаем проценты
+			coordXprocent = coordX / furnitureWidth * 200;
+
+			if (!furniture.classList.contains('_init')) {
+				requestAnimationFrame(setMouseGalleryStyle);
+				furniture.classList.add('_init');
+			}
+		});
+	}
+
 }
 
 
